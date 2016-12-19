8904018a3848c4bea2e445cf400b8627784e3abc
Guilherme Coelho Piovesan <guilhermecoelhopiovesan@gmail.com>
2016-12-19 14:48:37 -0200

    Create more child and index routes

--- app/app.js
+++ app/app.js
@@ -7,7 +7,10 @@ class App extends Component {
       <Router history={hashHistory}>
         <Route path='/' component={Container}>
           <IndexRoute component={Home} />
-          <Route path='contato' component={Contato} />
+          <Route path='contato' component={Contato}>
+            <IndexRoute component={Localizacao} />
+            <Route path='filiais' component={Filiais} />
+          </Route>
           <Route path='*' component={NotFound} />
         </Route>
       </Router>
@@ -16,8 +19,18 @@ class App extends Component {
 }
 
 const Home = () => <h1>Página Inicial</h1>
-const Contato = () => <h1>Contato</h1>
+const Contato = (props) => (
+  <div>
+    <Link to ='/contato'>Localização</Link>&nbsp;
+    <Link to ='/contato/filiais'>Filiais</Link>
+    <h1>Contato</h1>
+    {props.children}
+  </div>
+)
 const NotFound = () => (<h1>Erro 404 - A página não foi encontrada!</h1>)
+
+const Localizacao = () => (<h3>Localização</h3>)
+const Filiais = () => (<h3>Filiais</h3>)
 
 const Container = (props) => (
   <div>
